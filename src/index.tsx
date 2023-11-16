5d5485eec6a47d37cb513faf092beee48a5f8027
Stloz <stloz.serg@gmail.com>
2023-11-16 17:46:05 +0200

    readonly

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App2";
-import App1 from "./App1";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
