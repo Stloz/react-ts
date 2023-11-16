34575a0a375a5da528bb2e3dd70dcc2f74b218fa
Stloz <stloz.serg@gmail.com>
2023-11-16 18:08:39 +0200

    tuple

--- src/App7.tsx
+++ src/App7.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "./App.css";
 
-enum Color {
-  Red,
-  Green,
-  Blue,
+enum COLOR {
+  RED = "red",
+  GREEN = "green",
+  BLUE = "blue",
 }
 
-console.log(Color);
+console.log(COLOR);
 
-function displayColor(color: Color): void {
-  if (color === Color.Red) console.log("The selected color id red.");
-  else if (color === Color.Green) console.log("The selected color id green.");
-  else if (color === Color.Blue) console.log("The selected color id blue.");
+function displayColor(color: COLOR): void {
+  if (color === COLOR.RED) console.log("The selected color id red.");
+  else if (color === COLOR.GREEN) console.log("The selected color id green.");
+  else if (color === COLOR.BLUE) console.log("The selected color id blue.");
   else console.log("Invalid color");
 }
 
-const selectedColor: Color = Color.Green;
+const selectedColor: COLOR = COLOR.GREEN;
 displayColor(selectedColor);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> Hello </header>
+      <header className="App-header"> Hello enum </header>
     </div>
   );
 }
