741f958f2b081d052ccf9c33b139d1b74c1f9441
Stloz <stloz.serg@gmail.com>
2023-11-20 21:25:22 +0200

    React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App22-useRef";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { create } from "domain";
 import { createContext } from "vm";
