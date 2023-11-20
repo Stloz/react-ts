6365f9e2cf7363410c3f5f2ea2089f1b75a7e67c
Stloz <stloz.serg@gmail.com>
2023-11-20 19:35:25 +0200

    React.Element

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App17";
+import App from "./App18";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const Child: React.FC<{ style: React.CSSProperties }> = ({ style }) => (
+  <div style={style}>Child prop</div>
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App test={<div>Hi</div>}>
+      <Child style={{ color: "red" }} />
+    </App>
   </React.StrictMode>
 );
 
